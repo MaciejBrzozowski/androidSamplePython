c0eeb763dce0bfab43f31282e6be84d808e524ce
mbrzozowski <maciej.brzozowski@upaid.pl>
2019-05-20 16:02:17 +0200

    init commit

---
+++ app/src/main/python/main.py
@@ -0,0 +1,11 @@
+from android.os import Bundle
+from android.support.v7.app import AppCompatActivity
+from com.example.myapplication import R
+from java import jvoid, Override, static_proxy
+
+class MainActivity(static_proxy(AppCompatActivity)):
+
+    @Override(jvoid, [Bundle])
+    def onCreate(self, state):
+        AppCompatActivity.onCreate(self, state)
+        self.setContentView(R.layout.activity_main)
